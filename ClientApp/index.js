a4791637e79e32085b79107b314bd204388ae5b8
Mohammad Solgi <m.solgi@apasaico.local>
2022-01-04 10:51:13 +0330

    correction app name in prod

--- ClientApp/index.js
+++ ClientApp/index.js
@@ -5,9 +5,7 @@ import './index.css'
 
 // Now we can render our application into it
 let root = document.createElement('div')
-
 root.id = 'root'
 document.body.appendChild(root)
-
 // Now we can render our application into it
 render(<App />, document.getElementById('root'))
